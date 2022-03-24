dc6b6dc9548c7555db4ae7e12daf2b9fcf7a8bc5
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-03-24 19:21:16 +0900

    add handler

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState<string>('')
+
+  const goalInputHandler = (enteredText: string) => {
+    setEnteredGoalText(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    console.info(enteredGoalText)
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" onPress={() => console.info('通過')} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
@@ -25,6 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: '#cccccc',
@@ -34,7 +49,7 @@ const styles = StyleSheet.create({
     borderColor: '#cccccc',
     width: '70%',
     marginRight: 8,
-    padding: 9,
+    padding: 8,
   },
   goalsContainer: {
     flex: 5,
